a2b37933125a3292adc7181e7bb7495e802b66eb
TNarisara <68070276@kmitl.ac.th>
2026-03-12 12:32:51 +0700

    edit รวม app กับ calculator

--- App.java
+++ App.java
@@ -1,17 +1,31 @@
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.*;
 
-public class App {
+public class App extends JPanel {
+    
+   
+
+    Image bg = new ImageIcon("bg.jpg").getImage();
     public static void main(String[] args) {
-
+        
         JFrame frame = new JFrame("My Game");
-        frame.setSize(1200, 700);
+        frame.setSize(1920, 1080);
+
+     
+        JLabel image;
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        Calculator panel = new Calculator();
-
+        App panel = new App();
+        // JLabel bg = new JLabel(new ImageIcon("bg.jpg"));
+        // panel.add(bg);
+        // Calculator panel = new Calculator();
+        frame.setLocationRelativeTo(null);
         frame.add(panel);
         frame.setVisible(true);
     }
+    protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+
+    g.drawImage(bg, 0, 0, getWidth(), getHeight(), this);
+}
 }
