a775a22164620f14e8257edaa7a1a0535118d039
TNarisara <68070276@kmitl.ac.th>
2026-03-12 15:33:44 +0700

    rec area ปุ่มกด

--- App.java
+++ App.java
@@ -2,25 +2,34 @@ import java.awt.*;
 import javax.swing.*;
 
 public class App extends JPanel {
-    Image bg = new ImageIcon("bg.jpg").getImage();
-    public static void main(String[] args) {
-        
-        JFrame frame = new JFrame("My Game");
-        frame.setSize(1920, 1080);
 
-     
-        JLabel image;
+    Image bg = new ImageIcon("bg.jpg").getImage();
+    Calculator calc = new Calculator();
+
+    public App(){
+        setLayout(new BorderLayout());
+        add(calc, BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("My Game");
+        frame.setSize(1920,1080);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         App panel = new App();
+
+        frame.add(panel);
+
         frame.setLocationRelativeTo(null);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.add(panel);
         frame.setVisible(true);
     }
-    protected void paintComponent(Graphics g) {
-    super.paintComponent(g);
 
-    g.drawImage(bg, 0, 0, getWidth(), getHeight(), this);
-}
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        g.drawImage(bg,0,0,getWidth(),getHeight(),this);
+    }
 }
