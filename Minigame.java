3127346cd195e9f05f80787133c813d875351b9b
usernametapp <68070230@kmitl.ac.th>
2026-03-12 12:33:43 +0700

    Create Minigame.java
    abstract minigame

---
+++ Minigame.java
@@ -0,0 +1,18 @@
+public abstract class Minigame {
+    protected boolean isFinish;
+    protected boolean isPass;
+
+    public void setFinish(boolean isFinish){
+        this.isFinish = isFinish;
+    }
+    public boolean getFinish(){
+        return isFinish;
+    }
+    public void setPass(boolean isPass){
+        this.isPass = isPass;
+    }
+    public boolean getPass(){
+        return isPass;
+    }
+    public abstract void play();
+}
