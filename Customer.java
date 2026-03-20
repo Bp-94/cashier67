723b9a14c5ac0ef6a205a6da6aa28a7d0aa2baf2
WARIN <147617436+warinnnn0927@users.noreply.github.com>
2026-03-20 16:45:49 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,16 +1,26 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 import java.util.*;
-public class Customer {
+
+public class Customer implements Obserable, ActionListener {
+    private ArrayList<Observer> observers = new ArrayList<>();
     private ArrayList<Goods> goodsToBuy;
     private int countWrong;
     private boolean haveAlcohol;
     private boolean haveCoupon;
-    private int discount;
-//    private String CustomerImage;
-    public Customer() {
+    private int age;
+    private double discount;
+    private double finalPrice;
+    private double payment;
+//    private ImageIcon customer;
+    
+    public Customer(Game game) {
         Random random = new Random();
-        goodsToBuy = new ArrayList<Goods>();
-        int amountToBuy = random.nextInt(10);
+        goodsToBuy = new ArrayList<>();
+        int amountToBuy = random.nextInt(10) + 1;
         haveAlcohol = false;
+        finalPrice = 0;
         for (int i = 0; i < amountToBuy; i++) {
             int randomGoods = random.nextInt(Game.goodsList.length);
             Goods g = Game.goodsList[randomGoods];
@@ -18,35 +28,90 @@ public class Customer {
                 haveAlcohol = true;
             }
             goodsToBuy.add(g);
+            finalPrice += g.getPrice();
         }
         if (haveAlcohol) {
-//            แสดงภาพ ID_card กดแล้วให้ New ID_card
+            int[] ages = {18, 19, 21, 25};
+            age = ages[random.nextInt(ages.length)];
+//            add ภาพ ID_Card บนโต๊ะ
+//            add ActionListener ให้ ID_Card
         }else {
-            haveCoupon = random.nextDouble() < 0.3;
+            haveCoupon = random.nextDouble() < 0.3; //มีโอกาส 30%
             if (haveCoupon) {
-                
+                int[] discounts = {25, 50};
+                discount = discounts[random.nextInt(discounts.length)];
+                finalPrice -= finalPrice * (discount / 100);
+//                add ภาพ Coupon บนโต๊ะ
             }
         }
         countWrong = 0;
-//        int imageIndex = random.nextInt(???);
-//        CustomerImage = ("" + imageIndex);
+        this.add(game);
+//        int imageIndex = random.nextInt(5) + 1;
+//        CustomerImage = ("CustomerImage" + imageIndex);
     }
-//    public void checkPrice(double playerInput) {
-//        double sum = 0;
-//        for (Goods g : goodsToBuy) {
-//            sum += g.getPrice();
+    public boolean checkPrice(double playerInput) {
+        if (Math.abs(playerInput - finalPrice) < 0.01) { //double มีโอกาส ค่าไม่เท่ากันทั้งที่ควรเท่า เลยใช้วิธีนี้เทีบ double
+            return true;
+        }
+        countWrong += 1;
+        if (countWrong == 1) {
+            payment *= 0.75;
+//            ใส่ภาพเครื่องหมาย
+        }else if (countWrong == 2) {
+            payment *= 0.5;
+//            ใส่ภาพเครื่องหมาย
+        }else {
+            this.leave();
+        }
+        return false;
+    }
+    public void leave() {
+        //ภาพลูกค้าเลื่อนออก 
+        this.notifyObserver("CustomerLeft");
+    }
+    public void showID_Card() {
+        //สร้างหน้าต่าง
+        //เพิ่มภาพบัตรที่อายุตามที่สุ่มได้ลงในหน้าต่าง (age)
+        //ปุ่มขายหรือไม่ขาย
+        //โชว์
+    }
+    public void showCoupon() {
+        //แสดงภาพของคูปองตามที่สุ่มได้ (discount)
+    }
+    
+    //Setter&Getter
+    public double getPayment() {
+        return payment;
+    }
+    public int getCountWrong() {
+        return countWrong;
+    }
+    //
+    
+    //Obserable
+    @Override
+    public void add(Observer observer) {
+        observers.add(observer); 
+    } 
+    @Override
+    public void remove(Observer observer) {
+        observers.remove(observer); 
+    } 
+    @Override
+    public void notifyObserver(String message) {
+        for (Observer observer : observers){
+            observer.update(message); } 
+    }
+    //
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+//        if (ae.getSource().equals(ID_Card)) {
+//            this.showID_Card();
+//        }else if (ae.getSource().equals(Coupon)) {
+//            this.showCoupon();
 //        }
-//        if (((haveCoupon) && ((sum - sum*(ส่วนลดคูปอง)) == sum)) || ((!haveCoupon) && (playerInput == sum))) {
-//            
-//        }else {
-//            countWrong += 1;
-//            if (countWrong == 1) {
-//                
-//            }else if (countWrong == 2) {
-//                
-//            }else {
-//                
-//            }
-//        }
-//    }
+//        ปุ่มไม่ขายเหล้าให้ -> เดินออกไปเลย
+                
+    }
 }
