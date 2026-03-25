852144306304e4801d0f3bcb0d50369dc2a0c515
usernametapp <68070230@kmitl.ac.th>
2026-03-25 14:45:45 +0700

    update guessGoods

--- Minigame.java
+++ Minigame.java
@@ -1,13 +1,6 @@
 public abstract class Minigame {
-    protected boolean isFinish;
     protected boolean isPass;
 
-    public void setFinish(boolean isFinish){
-        this.isFinish = isFinish;
-    }
-    public boolean getFinish(){
-        return isFinish;
-    }
     public void setPass(boolean isPass){
         this.isPass = isPass;
     }
